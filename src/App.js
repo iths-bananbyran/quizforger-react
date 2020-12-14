6c790ec27acf03ec47f930d2344cd03c0a11fd83
iths-anna-boberg <amboberg@gmail.com>
2020-12-14 15:56:00 +0100

    render latest posts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.scss';
+import Header from'./components/Header';
+import Startpage from './components/Startpage';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Startpage />
       <h1>Quizforge</h1>
     </div>
   );
