ca6e0f2e1ab5f7566dc60018af9ae4ec4244891a
iths-anna-boberg <amboberg@gmail.com>
2020-12-30 15:58:13 +0100

    tryout

--- src/components/Omoss.jsx
+++ src/components/Omoss.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+
 const Omoss = ()=>{
 
+    const [content, setContent] = useState(null);
+
+    useEffect( ()=> {
+
+        const url = 'https://www.peowstudio.com/quizforge/wp-json/wp/v2/pages?slug=contact';
+
+        const fetchContent = async ()=>{
+            await fetch(url)
+                    .then(result => result.json())
+                    .then(result=>{
+                        setContent(result)
+                    })
+                    .catch(e => console.error(e))
+        }
+
+        fetchContent();
+
+    }, [])
+
+    console.log(content)
     return(
-        <p>Om oss!</p>
+        <>
+        {content && content[0].content.rendered}
+        </>
+        
     )
 }
 export default Omoss;
